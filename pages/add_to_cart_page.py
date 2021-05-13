3f8ae019ecf2917b54909f21b6671760e6ca20a7
zdydekz123 <zdybek1997@gmail.com>
2021-05-13 21:55:25 +0000

    update

--- pages/add_to_cart_page.py
+++ pages/add_to_cart_page.py
@@ -13,18 +13,21 @@ class AddItemToCart(BasePage):
 
     @allure.step('Weryfikacja zgodności tytułu sklepu')
     def check_page_title(self):
+        self.logger.info('[LOGS] Weryfikacja zgodności tytułu sklepu')
         get_title = self.driver.title
         allure.attach(self.driver.get_screenshot_as_png(), name='check_page_title', attachment_type=allure.attachment_type.PNG)
         return get_title
 
     @allure.step('Czy wyszukiwarka strony jest widoczna')
     def is_search_field_displayed(self):
+        self.logger.info('[LOGS] Czy wyszukiwarka strony jest widoczna')
         search_field = self.driver.find_element(*SearchEngineLocators.SEARCH_FIELD)
         allure.attach(self.driver.get_screenshot_as_png(), name='is_search_field_displayed', attachment_type=allure.attachment_type.PNG)
         return search_field.is_displayed()
 
     @allure.step('Weryfikacja poprawnej wartości placeholdera')
     def checkout_placeholder_value(self):
+        self.logger.info('[LOGS] Weryfikacja poprawnej wartości placeholdera')
         wait = WebDriverWait(self.driver, 40)
         placeholder = wait.until(EC.presence_of_element_located(SearchEngineLocators.SEARCH_FIELD))
         allure.attach(self.driver.get_screenshot_as_png(), name='checkout_placeholder_value', attachment_type=allure.attachment_type.PNG)
@@ -32,31 +35,36 @@ class AddItemToCart(BasePage):
 
     @allure.step('Kliknięcie w puste pole wyszukiwarki')
     def click_on_search_input(self):
+        self.logger.info('[LOGS] Kliknięcie w puste pole wyszukiwarki')
         search_field = self.driver.find_element(*SearchEngineLocators.SEARCH_FIELD)
         search_field.click()
         allure.attach(self.driver.get_screenshot_as_png(), name='click_on_search_input', attachment_type=allure.attachment_type.PNG)
 
     @allure.step('Wpisana wartość w pole wyszukiwarki to "{1}"')
-    def type_proper_value(self, item_name):
+    def type_proper_value(self, items_name):
+        self.logger.info('[LOGS] Wpisana wartość w wyszukiwarce to {itemsname}'.format(itemsname=items_name))
         search_value = self.driver.find_element(*SearchEngineLocators.SEARCH_FIELD)
-        search_value.send_keys(item_name)
+        search_value.send_keys(items_name)
         allure.attach(self.driver.get_screenshot_as_png(), name='type_proper_value', attachment_type=allure.attachment_type.PNG)
-        return item_name
+        return items_name
 
     @allure.step('Czy przycisk zatwierdzenia jest dostępny')
     def is_submit_button_enabled(self):
+        self.logger.info('[LOGS] Czy przycisk zatwierdzenia jest dostępny')
         submit_value = self.driver.find_element(*SearchEngineLocators.SEARCH_SUBMIT)
         allure.attach(self.driver.get_screenshot_as_png(), name='is_submit_button_enabled',attachment_type=allure.attachment_type.PNG)
         return submit_value.is_enabled()
 
     @allure.step('Wyszukiwanie pożądanej wartości')
     def submit_typed_value(self):
+        self.logger.info('[LOGS] Wyszukiwanie pożądanej wartości')
         submit_value = self.driver.find_element(*SearchEngineLocators.SEARCH_SUBMIT)
         submit_value.click()
         allure.attach(self.driver.get_screenshot_as_png(), name='submit_typed_value', attachment_type=allure.attachment_type.PNG)
 
     @allure.step('Weryfikacji znalezionych produktów o podanej wartości')
     def checkout_searched_value(self):
+        self.logger.info('[LOGS] Weryfikacji znalezionych produktów o podanej wartości')
         try:
             result = self.driver.find_element(*SearchEngineLocators.NO_SEARCH_RESULTS).get_attribute('textContent')
             complement = self.driver.find_element(*SearchEngineLocators.RESULT_COMPLEMENT).get_attribute('textContent')
@@ -70,12 +78,14 @@ class AddItemToCart(BasePage):
 
     @allure.step('Wybranie danego produktu z listy')
     def select_available_item(self):
+        self.logger.info('[LOGS] Wybranie danego produktu z listy')
         item_visible = self.driver.find_elements(*AddToCartLocators.FIND_PRODUCT)
         item_visible[0].click()
         allure.attach(self.driver.get_screenshot_as_png(), name='select_available_item', attachment_type=allure.attachment_type.PNG)
 
     @allure.step('Czy okno kontynuacji zakupu jest dostępne')
     def is_continuation_popup_displayed(self):
+        self.logger.info('[LOGS] Czy okno kontynuacji zakupu jest dostępne')
         wait = WebDriverWait(self.driver, 40)
         order_popup = wait.until(EC.visibility_of_element_located(AddToCartLocators.ORDER_POPUP))
         allure.attach(self.driver.get_screenshot_as_png(), name='is_continuation_popup_displayed', attachment_type=allure.attachment_type.PNG)
@@ -83,6 +93,7 @@ class AddItemToCart(BasePage):
 
     @allure.step('Przejście do koszyka')
     def go_to_cart(self):
+        self.logger.info('[LOGS] Przejście do koszyka')
         order_popup = self.driver.find_element(*AddToCartLocators.ORDER_POPUP)
         button_gtc = self.driver.find_element(*AddToCartLocators.GO_TO_CART)
         if order_popup.is_displayed():
@@ -94,6 +105,7 @@ class AddItemToCart(BasePage):
 
     @allure.step('Weryfikacja ilości produktów dodanych do koszyka')
     def check_cart_value(self):
+        self.logger.info('[LOGS] Weryfikacja ilości produktów dodanych do koszyka')
         wait = WebDriverWait(self.driver, 40)
         value_cart = wait.until(EC.visibility_of_element_located(VerificationOrder.VERIFY_PRODUCT))
         go_delivery = wait.until(EC.element_to_be_clickable(VerificationOrder.GO_TO_DELIVERY))
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
+import logging
+
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(__name__)
